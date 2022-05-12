60433bf375a61488344c06d91b9c922a47d89a72
Juan Hernandez <69637082+jphernandezr1@users.noreply.github.com>
2022-05-11 21:58:30 -0500

    jp

--- ProblemaP0.py
+++ ProblemaP0.py
@@ -1,15 +1,44 @@
 import sys
 
 
+def desarrollo(matriz, vis):
+    visitados=vis
+    cuantos=0
+    llaves= matriz.keys()
+    for llave in llaves:
+        if visitados[llave]==False:
+            cuantos+=1
+            visitados[llave]=True
+            listaAdyacencia= matriz[llave]
+            for elemento in listaAdyacencia:
+                visitados[elemento]=True
+        else:
+            pass
+    print(cuantos)
+
 numero_casos = int(sys.stdin.readline())
 for __ in range(numero_casos):
+    infy=-1
     case_list = list(map(int, sys.stdin.readline().split()))
-    np = 0
-    sp = 0
-    for n in case_list:
-    	if n % 2 == 0 :
-    		np=np+1
+    vis={}
+    matriz={}
+    for n in range (len(case_list)):
+        actual=int(case_list[n])
+        if n==0:
+            matriz[actual]= []
+            vis[actual]=False
+        else :
+            matriz[actual]= []
+            vis[actual]=False
+            i = n-1
+            while i > -1:
+                anterior=int(case_list[i])
+                if anterior>actual:
+                    pos=matriz[anterior]
+                    pos.append(actual)
+                i-=1
+    desarrollo(matriz,vis)
+    
 
 
-def desarrollo():
-    visitados=[False*np]
+
