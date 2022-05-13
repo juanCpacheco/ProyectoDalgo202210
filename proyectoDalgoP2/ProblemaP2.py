c41d8367c8af26c29e1bb22374dd5014a6e35d2a
sofiaduqueg <you@example.coms.duqueg@uniandes.edu.co>
2022-05-13 17:24:55 -0500

    Cambios finales

--- proyectoDalgoP2/ProblemaP2.py
+++ proyectoDalgoP2/ProblemaP2.py
@@ -1,1 +1,29 @@
-# Dearrollo entrega 2 proyecto dalgo
+#Sofía Duque Gómez - 201922178
+#Juan Camilo Pacheco - 201913666
+#Juan Pablo Hernández - 201912855
+
+import sys
+import time
+
+start = time.time()
+numero_casos = int(sys.stdin.readline())
+for caso in range(numero_casos):
+    case_list = list(map(int, sys.stdin.readline().split()))
+    cant = 0
+    while len(case_list) > 0:
+        actual = case_list[0]
+        case_list.remove(actual)
+        j = 0
+        while j < len(case_list):
+            actual2 = case_list[j]
+            if actual > actual2:
+                case_list.remove(actual2)
+            else:
+                j += 1
+        cant += 1
+
+    # print(componentes)
+    print(cant)
+    
+end = time.time()
+print("\nEl tiempo que se demoro fue de", end - start, "segundos.")
