b50484671f98264e8aebf3f627d660d400a94769
Juan Camilo Pacheco <jc.pacheco@uniandes.edu.co>
2022-05-13 17:12:08 -0500

    intento3

--- intento3.py
+++ intento3.py
@@ -3,16 +3,18 @@ import sys
 numero_casos = int(sys.stdin.readline())
 for caso in range(numero_casos):
     case_list = list(map(int, sys.stdin.readline().split()))
-    visitados = []
     cant = 0
-    for i in range(len(case_list)):
-        actual = case_list[i]
-        if actual not in visitados:
-            visitados.append(actual)
-            for j in range(i+1, len(case_list)):
-                actual2 = case_list[j]
-                if actual > actual2 and actual2 not in visitados:
-                    visitados.append(actual2)
-            cant += 1
+    while len(case_list) > 0:
+        actual = case_list[0]
+        case_list.remove(actual)
+        j = 0
+        while j < len(case_list):
+            actual2 = case_list[j]
+            if actual > actual2:
+                case_list.remove(actual2)
+            else:
+                j += 1
+        cant += 1
+
     # print(componentes)
     print(cant)
